46185601214af8911e9ba4a2a8159d5cec4c18ee
Kate Lawrence-Gupta <39601928+klawrencegupta-splunk@users.noreply.github.com>
2021-02-01 13:53:18 -0500

    updates
    field extractions added and xml updated with job name dashboard
    1 more could be added for job type but with the name that could also be the case

--- fio_analyzer_agent/bin/fio_wrapper.sh
+++ fio_analyzer_agent/bin/fio_wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/bin/python $SPLUNK_HOME/etc/apps/fio_analyzer_agent/bin/fio_splunk_hec.py $SPLUNK_HOME/etc/apps/fio_analyzer_agent/local/randomrw_fio.job "b82915de-8c4b-4924-ba4e-ee956bb3435f"  "https://34.201.48.60:8088"
+#/usr/bin/python $SPLUNK_HOME/etc/apps/fio_analyzer_agent/bin/fio_splunk_hec.py $SPLUNK_HOME/etc/apps/fio_analyzer_agent/local/randomrw_fio.job "b82915de-8c4b-4924-ba4e-ee956bb3435f"  "https://10.202.33.135:8088"
+for x in $(ls -1 $SPLUNK_HOME/etc/apps/fio_analyzer_agent/local/*.job); do /usr/bin/python $SPLUNK_HOME/etc/apps/fio_analyzer_agent/bin/fio_splunk_hec.py $x "b82915de-8c4b-4924-ba4e-ee956bb3435f" "https://10.202.33.135:8088"; done
